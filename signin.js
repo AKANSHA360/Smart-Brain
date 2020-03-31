b6d5ce9ce290bf17be3842610b3d25950cf45660
AKANSHA360 <59160431+AKANSHA360@users.noreply.github.com>
2020-03-31 12:18:01 +0530

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,45 @@
+const handleSignin = (req, res, db, bcrypt) =>{
+	const { email , password} = req.body;
+	if(!email || !password )
+	{
+		return res.status(400).json('incorrect form submission');
+	}
+			// Load hash from your password DB.
+			/*bcrypt.compare("apples", '$2a$10$5O9pbFE6Bqym4UA81dDUIOLQwwEG3088SLJfZ7wGovi7Vd2i2UUqa', function(err, res) {
+			     //res == true
+			     console.log('first guess' , res);
+			});
+			bcrypt.compare("veggies", '$2a$10$5O9pbFE6Bqym4UA81dDUIOLQwwEG3088SLJfZ7wGovi7Vd2i2UUqa', function(err, res) {
+			     //res = false
+			     console.log('second guess' , res);
+			});
+
+
+			if(req.body.email===database.users[0].email &&
+				req.body.password === database.users[0].password){
+				res.json(database.users[0]);
+			}
+			else{
+				res.status(400).json('error logging in');
+			}*/
+			db.select('email' , 'hash').from('login')
+			.where('email' , '=' , email)
+			.then(data => {
+				const isValid = bcrypt.compareSync(password , data[0].hash);
+				if (isValid) {
+					return db.select('*').from('users')
+					.where('email' , '=' , email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+				}else{
+				res.status(400).json('wrong credentials')
+			}
+			})
+			.catch(err => res.status(400).json('wrong credentials'))
+}
+
+module.exports = {
+	handleSignin : handleSignin
+}
